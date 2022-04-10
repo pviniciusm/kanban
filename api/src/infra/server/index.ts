0b529d9040021770320f3af0ceaa27d33976541b
pviniciusm <pvinics@gmail.com>
2022-04-10 13:58:47 -0300

    chore: set routes on express server init

--- api/src/infra/server/index.ts
+++ api/src/infra/server/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import { setRoutes } from "../routes";
 
 export class Server {
     private static _app = express();
 
     static init() {
         Server._app.use(cors());
+        setRoutes(Server._app);
+
         Server._app.listen(process.env.API_PORT || 8081, () =>
             console.log("Server is running")
         );

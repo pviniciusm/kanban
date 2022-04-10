93d81d8a15de231f774ae7bae71ccb7e365580bc
pviniciusm <pvinics@gmail.com>
2022-04-10 10:16:50 -0300

    feat: create card controller class + tests

---
+++ api/src/presentation/controllers/card/create-card-controller.spec.ts
@@ -0,0 +1,69 @@
+import { List } from "./../../../domain/entities/card";
+import { HttpRequest } from "./../index";
+import { MockCardRepository } from "../../../domain/repositories/mock/mock-card-repository";
+import { CreateCardUseCase } from "../../../domain/usecases";
+import { CreateCardController } from "./create-card-controller";
+
+describe("create card controller tests", () => {
+    const makeSut = () => {
+        const repository = new MockCardRepository();
+        const useCase = new CreateCardUseCase(repository);
+        const sut = new CreateCardController(useCase);
+
+        const request: HttpRequest = {
+            body: {
+                title: "any_title",
+                content: "any_content",
+                list: List.toDo.toString(),
+            },
+        };
+
+        return { sut, request: { ...request } };
+    };
+
+    test("should return code 400 if no title is provided", async () => {
+        const { sut, request } = makeSut();
+
+        request.body.title = undefined;
+
+        const response = await sut.handle(request);
+        expect(response).toBeDefined();
+        expect(response.code).toEqual(400);
+        expect(response.ok).toBeFalsy();
+        expect(response.identifier).toEqual("MissingFieldError");
+    });
+
+    test("should return code 400 if no content is provided", async () => {
+        const { sut, request } = makeSut();
+
+        request.body.content = undefined;
+
+        const response = await sut.handle(request);
+        expect(response).toBeDefined();
+        expect(response.code).toEqual(400);
+        expect(response.ok).toBeFalsy();
+        expect(response.identifier).toEqual("MissingFieldError");
+    });
+
+    test("should return code 400 if no list is provided", async () => {
+        const { sut, request } = makeSut();
+
+        request.body.list = undefined;
+
+        const response = await sut.handle(request);
+        expect(response).toBeDefined();
+        expect(response.code).toEqual(400);
+        expect(response.ok).toBeFalsy();
+        expect(response.identifier).toEqual("MissingFieldError");
+    });
+
+    test("should return code 201 if request is ok", async () => {
+        const { sut, request } = makeSut();
+
+        const response = await sut.handle(request);
+        expect(response).toBeDefined();
+        expect(response.code).toEqual(201);
+        expect(response.ok).toBeTruthy();
+        expect(response.identifier).toEqual("Success");
+    });
+});
---
+++ api/src/presentation/controllers/card/create-card-controller.ts
@@ -0,0 +1,32 @@
+import { CreateCardUseCase } from "../../../domain/usecases";
+import { error } from "../../util/handlers/handle-error";
+import { MissingFieldError } from "../../util/errors/missing-field-error";
+import { Controller, HttpRequest, ResponseData } from "../index";
+import { success } from "../../util/handlers/handle-success";
+
+export class CreateCardController implements Controller {
+    constructor(private useCase: CreateCardUseCase) {}
+
+    async handle(request: HttpRequest): Promise<ResponseData> {
+        try {
+            const { title, content, list } = request.body;
+
+            if (!title) {
+                throw new MissingFieldError("title");
+            }
+
+            if (!content) {
+                throw new MissingFieldError("content");
+            }
+
+            if (!list) {
+                throw new MissingFieldError("list");
+            }
+
+            const result = this.useCase.run({ ...request.body });
+            return success(result, "Create Card", 201);
+        } catch (e) {
+            return error(e);
+        }
+    }
+}

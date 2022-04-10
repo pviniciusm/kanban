7b95d7f29bc0fcb0c42fb8c86181016ef1f2ebc8
pviniciusm <pvinics@gmail.com>
2022-04-10 13:56:44 -0300

    feat: factory method to create controller instances

---
+++ api/src/presentation/util/factory/card-controllers.factory.ts
@@ -0,0 +1,26 @@
+import { ListCardsUseCase } from "./../../../domain/usecases/card/list-cards/list-cards-usecase";
+import { CreateCardUseCase } from "./../../../domain/usecases/card/create-card/create-card-usecase";
+import { CardRepository } from "../../../infra/database/typeorm/repository";
+import { ListCardsController } from "../../controllers/card/list-card/list-cards-controller";
+import { CreateCardController } from "../../controllers/card/create-card/create-card-controller";
+
+export const makeControllers = () => {
+    // Repository - Typeorm
+    const cardRepository = new CardRepository();
+
+    // Usecases
+    const createCardUseCase = new CreateCardUseCase(cardRepository);
+    const listCardsUseCase = new ListCardsUseCase(cardRepository);
+
+    // Controllers
+    const createCardControllerImpl = new CreateCardController(
+        createCardUseCase
+    );
+
+    const listCardsControllerImpl = new ListCardsController(listCardsUseCase);
+
+    return {
+        create: createCardControllerImpl,
+        list: listCardsControllerImpl,
+    };
+};

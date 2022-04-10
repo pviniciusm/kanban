bc348a863a78f7e174f0f06956faea5c739a9f9b
pviniciusm <pvinics@gmail.com>
2022-04-10 10:10:33 -0300

    feat: list cards use case tests

---
+++ api/src/domain/usecases/card/list-cards/list-cards-usecase.spec.ts
@@ -0,0 +1,45 @@
+import { v4 } from "uuid";
+
+import { UseCase } from "./../../index";
+import { ICardRepository } from "../../../repositories/card-repository";
+import { List, ICard } from "../../../entities/card";
+import { MockCardRepository } from "../../../repositories/mock/mock-card-repository";
+import {
+    getMockCardData,
+    MockCardRepositoryWithData,
+} from "./../../../repositories/mock/mock-card-repository";
+
+class ListCardsUseCase implements UseCase {
+    constructor(private repository: ICardRepository) {}
+
+    async run(): Promise<ICard[]> {
+        return this.repository.listAll();
+    }
+}
+
+describe("create card use case tests", () => {
+    const makeSut = (mockRepository?: ICardRepository) => {
+        const repository = new MockCardRepository();
+        const sut = new ListCardsUseCase(mockRepository ?? repository);
+        return { sut };
+    };
+
+    test("should return a list", async () => {
+        const { sut } = makeSut();
+
+        const result = await sut.run();
+        expect(Array.isArray(result)).toBe(true);
+    });
+
+    test("should return a list of cards if DB contains a card", async () => {
+        const card: ICard = getMockCardData()[0];
+
+        const repository = new MockCardRepositoryWithData();
+        const { sut } = makeSut(repository);
+
+        const result = await sut.run();
+        expect(Array.isArray(result)).toBe(true);
+        expect(result).toHaveLength(1);
+        expect(result[0]).toMatchObject(card);
+    });
+});

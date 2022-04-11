d6cb4952a802fe9c4e59e7d9fae4b1d5c8e67f6f
pviniciusm <pvinics@gmail.com>
2022-04-10 22:36:49 -0300

    refactor: adjust express server to use json data

--- api/src/infra/server/index.ts
+++ api/src/infra/server/index.ts
@@ -1,12 +1,13 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
-import { setRoutes } from "../routes";
+import { setRoutes } from "./routes";
 
 export class Server {
     private static _app = express();
 
     static init() {
         Server._app.use(cors());
+        Server._app.use(json());
         setRoutes(Server._app);
 
         Server._app.listen(process.env.API_PORT || 8081, () =>

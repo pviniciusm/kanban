0405cb1be702b0a8b46fafbf0b6b3ed67bea8bef
pviniciusm <pvinics@gmail.com>
2022-04-11 17:05:37 -0300

    feat: add update and delete controller instances to card factory

--- api/src/presentation/util/factory/card-controllers-factory.ts
+++ api/src/presentation/util/factory/card-controllers-factory.ts
@@ -1,8 +1,12 @@
+import { DeleteCardUseCase } from "./../../../domain/usecases/card/delete-card/delete-card-usecase";
+import { UpdateCardUseCase } from "./../../../domain/usecases/card/update-card/update-card-usecase";
 import { ListCardsUseCase } from "../../../domain/usecases/card/list-cards/list-cards-usecase";
 import { CreateCardUseCase } from "../../../domain/usecases/card/create-card/create-card-usecase";
 import { CardRepository } from "../../../infra/database/typeorm/repository";
 import { ListCardsController } from "../../controllers/card/list-card/list-cards-controller";
 import { CreateCardController } from "../../controllers/card/create-card/create-card-controller";
+import { DeleteCardController } from "../../controllers/card/delete-card/delete-card-controller";
+import { UpdateCardController } from "../../controllers/card/update-card/update-card-controller";
 
 export const makeControllers = () => {
     // Repository - Typeorm
@@ -11,6 +15,8 @@ export const makeControllers = () => {
     // Usecases
     const createCardUseCase = new CreateCardUseCase(cardRepository);
     const listCardsUseCase = new ListCardsUseCase(cardRepository);
+    const updateCardsUseCase = new UpdateCardUseCase(cardRepository);
+    const deleteCardsUseCase = new DeleteCardUseCase(cardRepository);
 
     // Controllers
     const createCardControllerImpl = new CreateCardController(
@@ -18,9 +24,17 @@ export const makeControllers = () => {
     );
 
     const listCardsControllerImpl = new ListCardsController(listCardsUseCase);
+    const deleteCardControllerImpl = new DeleteCardController(
+        deleteCardsUseCase
+    );
+    const updateCardControllerImpl = new UpdateCardController(
+        updateCardsUseCase
+    );
 
     return {
         create: createCardControllerImpl,
         list: listCardsControllerImpl,
+        update: updateCardControllerImpl,
+        delete: deleteCardControllerImpl,
     };
 };

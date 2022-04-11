0544fc40f78b8ec05867d4adc7d5743e867191bc
pviniciusm <pvinics@gmail.com>
2022-04-10 22:36:12 -0300

    feat: middleware to control jwt token on logged requests

---
+++ api/src/infra/server/middlewares/auth-middleware.ts
@@ -0,0 +1,18 @@
+import { validateJwt } from "./../../../presentation/util/helpers/jwt-validator";
+import { NextFunction, Request, Response } from "express";
+import { error } from "../../../presentation/util/handlers/handle-error";
+
+export const authorizationMid = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const payload = validateJwt(req.headers.authorization);
+        req.body.user = payload.user;
+    } catch (e) {
+        return res.status(403).send(error(e));
+    }
+
+    next();
+};

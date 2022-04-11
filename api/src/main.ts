721930832aca9e4f704cdf4dbee0686e61bac49d
pviniciusm <pvinics@gmail.com>
2022-04-10 22:37:28 -0300

    chore: dotenv-safe configuration

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,4 +1,8 @@
 import { database } from "./infra/database/typeorm/connection";
 import { Server } from "./infra/server";
+import safe from "dotenv-safe";
 
-database.init().then(Server.init);
+database
+    .init()
+    .then(Server.init)
+    .then(() => safe.config());

9d99f7cc7c8645e643fb4a7e561328b2bb327daf
pviniciusm <pvinics@gmail.com>
2022-04-11 16:59:37 -0300

    refactor: usecases exports on index file

--- api/src/domain/usecases/card/index.ts
+++ api/src/domain/usecases/card/index.ts
@@ -1,2 +1,4 @@
 export * from "./create-card/create-card-usecase";
-// export * from './list-cards/list-cards-usecase';
+export * from "./list-cards/list-cards-usecase";
+export * from "./update-card/update-card-usecase";
+export * from "./delete-card/delete-card-usecase";
